29b9ab4d89d16b775885d398864df4b400452241
Mikołaj Kaczmarek <mikolaj.kaczmarek@mesh-systems.com>
2024-01-23 00:59:21 +0100

    Day 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+char sex = 'F';
+int age = 25;
+
+if (sex == 'F' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
