e8cab44bf73a22996062fe1cb17413784c29e2c2
Mikołaj Kaczmarek <mikolaj.kaczmarek@mesh-systems.com>
2024-01-26 21:58:08 +0100

    Day 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> grades = new();
+
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+
+        public string Surname { get; private set; }
+
+        public int Age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return grades.Sum();
+            }
+        }
+
+        public void AddGrade(int grade)
+        {
+            this.grades.Add(grade);
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,18 +1,42 @@
-﻿int number = 1991;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
+﻿using ChallengeApp;
 
-for (char i = '0'; i <= '9'; i++)
+var employee1 = new Employee("Sandra", "Kaczmarek", 34);
+var employee2 = new Employee("Mikołaj", "Kaczmarek", 33);
+var employee3 = new Employee("Łukasz", "Kaczmarek", 99);
+
+employee1.AddGrade(1);
+employee1.AddGrade(2);
+employee1.AddGrade(3);
+employee1.AddGrade(4);
+employee1.AddGrade(5);
+
+employee2.AddGrade(3);
+employee2.AddGrade(4);
+employee2.AddGrade(5);
+employee2.AddGrade(6);
+employee2.AddGrade(7);
+
+employee3.AddGrade(5);
+employee3.AddGrade(6);
+employee3.AddGrade(7);
+employee3.AddGrade(8);
+employee3.AddGrade(9);
+
+List<Employee> employees = new()
 {
-    var letterCounter = 0;
+    employee1, employee2, employee3
+};
 
-    foreach (char letter in letters)
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
     {
-        if (i == letter)
-        {
-            letterCounter++;
-        }
+        employeeWithMaxResult = employee;
+        maxResult = employee.Result;
     }
-
-    Console.WriteLine(i + " => " + letterCounter);
 }
+
+Console.WriteLine(maxResult);
