f82679241a15a9e60309e85ef93c8f4e79afeca8
Mikołaj Kaczmarek <mikolaj.kaczmarek@mesh-systems.com>
2024-02-16 00:57:39 +0100

    Day 12

--- ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -7,19 +7,20 @@ namespace ChallengeApp.Tests
         {
             //Arrange
             var employee = new Employee("Jaros³aw", "Kaczafi");
-            employee.AddGrade(1);
-            employee.AddGrade(2);
-            employee.AddGrade(3);
-            employee.AddGrade(4);
+            employee.AddGrade("10");
+            employee.AddGrade(20);
+            employee.AddGrade(30);
+            employee.AddGrade('A');
 
             //Act
             var statistics = employee.GetStatistics();
 
             //Assert
             Assert.NotNull(statistics);
-            Assert.AreEqual(1, statistics.Min);
-            Assert.AreEqual(4, statistics.Max);
-            Assert.AreEqual(2.5, statistics.Average);
+            Assert.AreEqual(10, statistics.Min);
+            Assert.AreEqual(100, statistics.Max);
+            Assert.AreEqual(40, statistics.Average);
+            Assert.AreEqual('C', statistics.AverageLetter);
         }
     }
 }
